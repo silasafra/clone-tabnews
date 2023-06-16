6b153cbeed736b6e773f3266412f7eb8192d4c5a
Silas Afra <silasafra@gmail.com>
2023-06-16 01:51:12 +0000

    adicona página inicial

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home () {
+    return <h2>Meu bem, sei que você está chateada comigo, mas nosso amor é mais forte que qualquer desentendimento. Vamos superar isso juntos e fortalecer ainda mais nosso relacionamento. Te amo infinito🥺💓</h2>
+        
+    
+}
+
+export default Home; 
