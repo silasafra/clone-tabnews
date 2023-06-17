379f538585b55e138e75aa24fc5c5850abe16013
Silas Afra <silasafra@gmail.com>
2023-06-17 04:24:58 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h2>Meu bem, sei que você está chateada comigo, mas nosso amor é mais forte que qualquer desentendimento. Vamos superar isso juntos e fortalecer ainda mais nosso relacionamento. Te amo infinito🥺💓</h2>
+    return <h2>Meu bem, sei que você está chateada comigo, mas nosso amor é mais forte que qualquer desentendimento. Vamos superar isso juntos e fortalecer ainda mais nosso relacionamento. Te amo infinitooo🥺💓</h2>
         
     
 }
